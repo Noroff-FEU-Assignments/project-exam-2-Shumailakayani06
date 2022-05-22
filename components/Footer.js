6c149bceb17149d957caad4c48fb64efc0f2903a
Shumaila Kayani <shumailakayani059@gmail.com>
2022-05-22 12:07:47 +0200

    deleted existing content and started over again, added some basic layout

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <footer>
-            <p>Shumaila Kayani | Copyright 2022</p>
-        </footer>
-     );
-}
- 
-export default Footer;
--- utils/urls.js
+++
@@ -1,4 +0,0 @@
-export const URL = 'http://localhost:1337';
-export const BASE_URL = 'http://localhost:1337/api/establishments?populate=*';
-export const AUTH_URL = `${URL}api/auth/local`;
-export const AUTH_PATH = 'api/auth/local';
